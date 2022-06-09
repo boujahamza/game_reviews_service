deaf63e2b623ff630647cc1be5a9910f9a12e6e3
Boukedir Mohamed <bouukedir@outlook.com>
2022-06-09 02:06:01 +0100

    fix port

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose
     .then(() => console.log("MongoDB Connected ..."))
     .catch(err => console.log(err));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4001;
 
 
 app.post("/feed", async (req, res) => {
